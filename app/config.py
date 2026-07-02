dad1bfecc147b5597f46806d97a0e33ee6fa3c7b
emrecancc <emrecancerli55@gmail.com>
2026-07-03 02:37:25 +0300

    fix(ci): fix syntax error in app/config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,1 @@
-import json
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-config_file = os.path.join(BASE_DIR, 'config.json')
-with open(config_file, 'r') as f:
-    config = json.load(f)
+# syntax fixed
