2f4211ae64a3452e442c88aa148dc244e493dcee
emrecancc <emrecancerli55@gmail.com>
2026-07-03 06:54:51 +0300

    fix: increase performance test threshold to 400ms

--- test/performance.test.js
+++ test/performance.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('API performance', () => {
-  it('responds within 1250ms', async () => {
+  it('responds within 400ms', async () => {
     const start = Date.now();
     await request(app).get('/some-endpoint');
     const duration = Date.now() - start;
-    expect(duration).toBeLessThan(1250);
+    expect(duration).toBeLessThan(400);
   });
 });
